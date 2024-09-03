87ecaeb5008a0f6543d61d2c05e1932ae730433d
rmtonmoy <rmtonmoy232@gmail.com>
2024-09-03 12:46:24 -0400

    First stable version with depth based masking

--- engine_for_mgmae.py
+++ engine_for_mgmae.py
@@ -85,7 +85,7 @@ def train_one_epoch(model: torch.nn.Module,
             unnorm_images = images * std + mean  # in [0, 1]
             #nimgs = 2 * unnorm_images - 1.0  # in [-1, 1]
             nimgs = unnorm_images
-            mask_volume = build_mask_volume(nimgs, images, init_mask_map)
+            mask_volume = build_mask_volume(nimgs, images, init_mask_map, patch_size)
             if args.decoder_mask_type == 'none':
                 decode_masked_pos = ~mask_volume
 
@@ -191,10 +191,50 @@ def train_one_epoch(model: torch.nn.Module,
     print("Averaged stats:", metric_logger)
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
+def get_mask_map_on_depth(depth_info, height, width, patch_size):
+    
+    # depth_info.shape = 64 X 224 X 224 X 1
+    depth_info = depth_info.squeeze(-1)
+    batch_size = depth_info.shape[0]
+
+    depth_info = rearrange(
+        depth_info,
+        'b (h p0) (w p1) -> b (h w) (p0 p1)',   # b (224, 224) = b (14 * 16, 14, 16) -> b (196 , 224) = b (14 * 14, 16 * 16)
+        b = batch_size,
+        h = height,
+        w = width,
+        p0 = patch_size,
+        p1 = patch_size).to(torch.float32)
+    
+    depth_info = depth_info.mean(dim = 2)                   # b (14 * 14, 16 * 16) -> b (14 * 14) = b (196)
+    k = int(depth_info.size(1) * 0.5)                       # THRESHOLD
+    sorted_depth, _ = depth_info.sort(dim = 1, descending = True)
+    threshold_val = sorted_depth[:, k - 1].unsqueeze(1).repeat(1, height * width)         # shape = b (14 * 14)
+
+
+    mask = (depth_info >= threshold_val)
+    ret = torch.where(mask, torch.tensor(0.0), torch.tensor(1.0))           # b (14 * 14)
+    ret = ret.unsqueeze(-1).repeat(1, 1, patch_size * patch_size)           # b (14 * 14) , (16 * 16)
+
+    ret = rearrange(
+        ret,
+        'b (h w) (p0 p1) -> b (h p0) (w p1)',   # b (196 , 224) -> b (14 * 16, 14 * 16) = b (224, 224)
+        b = batch_size,
+        h = height,
+        w = width,
+        p0 = patch_size,
+        p1 = patch_size).to(torch.float32)    
+
+
+    return ret                                  # should be 64 X 224 X 224
+
+
 
 def get_build_mask_volume_func(flow_model, device, args):
-    def _build_mask_volume(nimgs, images, init_mask_map):
+    def _build_mask_volume(nimgs, images, init_mask_map, patch_size):
         mask_map_list = [init_mask_map]
+        print(f"Initial_mask_map shape = {init_mask_map.shape}")    # 64, 1, 224, 224
+
 
         B, C, T, H, W = nimgs.shape
         nimgs_transposed = nimgs.permute(2, 0, 1, 3, 4)
@@ -204,17 +244,35 @@ def get_build_mask_volume_func(flow_model, device, args):
         pipe = diffusers.MarigoldDepthPipeline.from_pretrained("prs-eth/marigold-depth-lcm-v1-0", variant="fp16", prediction_type = "depth", torch_dtype=torch.float16).to("cuda")
         for t in range(nimgs_transposed.size(0)):
 
+
+
+            depth = pipe(nimgs[t])
+            print(f"Shape of nimgs[t]: {nimgs[t].shape}")   # 64 x 3 x 224 x 224
+            print(f"Shape of depth : {depth[0].shape}")     # 64 x 224 X 224 x 1   
+            print("GOIGN TO CALCULATE MASK")
+            curr_mask = get_mask_map_on_depth(torch.from_numpy(depth[0]), int(H/16), int(W/16), patch_size) # 64 X 224 X 224
+            print(f"Shape of curr_mask: {curr_mask[0].shape}")
+ 
+
+            vis = pipe.image_processor.export_depth_to_16bit_png(depth.prediction)
+            #print(f"printing vis[0] -> {vis[0]}") 
+            #print(type(vis))
+
+            # Saving images
             for i in range(images[t].shape[0]):
                 image_np = images[t][i].cpu().permute(1,2,0).numpy()
                 image_np = (255 * (image_np - image_np.min()) / (image_np.max() - image_np.min())).astype('uint8')
+                mask_np = curr_mask[i].unsqueeze(-1).repeat(1,1,3)
+                print(f"Shape of mask_np: {mask_np.shape}")
+
+                #image_np = image_np * mask_np.numpy()               #do the masking
+ 
+                image_np = (255 * (image_np - image_np.min()) / (image_np.max() - image_np.min())).astype('uint8')
+
                 raw_image = Image.fromarray(image_np)
-                raw_image.save(f"output/raw_frames/frame{i}.png")
+                raw_image.save(f"output/raw_frames/frame{i}.png")      
 
-            depth = pipe(nimgs[t])
-            print(f"Shape of nimgs[t]: {nimgs[t].shape}") # 64 x 3 x H x W
-            print(f"Shape of depth : {depth.shape}")
-
-            vis = pipe.image_processor.export_depth_to_16bit_png(depth.prediction)
+                sys.exit(0)      
 
             for i in range(0, len(vis)):
                 vis[i].save(f"output/depth_maps/frame{i}.png")
